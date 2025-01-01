b55f4de986c89701e28ec02932bfecdc9061e1e4
dawid <dawid1bialek@gmail.com>
2025-01-01 23:18:45 +0100

    Init

---
+++ main.py
@@ -0,0 +1,186 @@
+# Fundamental classes
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+import visualkeras
+
+# Image related
+import cv2 as cv
+from PIL import Image
+
+#Performance Plot
+from sklearn import metrics
+from sklearn.metrics import f1_score, accuracy_score,confusion_matrix,classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# For the model and it's training
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+# Time
+import time
+import datetime
+
+def date_time(x):
+    if x==1:
+        return 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    if x==2:
+        return 'Timestamp: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
+    if x==3:
+        return 'Date now: %s' % datetime.datetime.now()
+    if x==4:
+        return 'Date today: %s' % datetime.date.today()
+
+def plot_performance(history=None, figure_directory=None, ylim_pad=[0, 0]):
+    xlabel = 'Epoch'
+    legends = ['Training', 'Validation']
+
+    plt.figure(figsize=(20, 5))
+
+    y1 = history.history['accuracy']
+    y2 = history.history['val_accuracy']
+
+    min_y = min(min(y1), min(y2))-ylim_pad[0]
+    max_y = max(max(y1), max(y2))+ylim_pad[0]
+
+    plt.subplot(121)
+
+    plt.plot(y1)
+    plt.plot(y2)
+
+    plt.title('Model Accuracy\n'+date_time(1), fontsize=17)
+    plt.xlabel(xlabel, fontsize=15)
+    plt.ylabel('Accuracy', fontsize=15)
+    plt.ylim(min_y, max_y)
+    plt.legend(legends, loc='upper left')
+    plt.grid()
+
+    y1 = history.history['loss']
+    y2 = history.history['val_loss']
+
+    min_y = min(min(y1), min(y2))-ylim_pad[1]
+    max_y = max(max(y1), max(y2))+ylim_pad[1]
+
+
+    plt.subplot(122)
+
+    plt.plot(y1)
+    plt.plot(y2)
+
+    plt.title('Model Loss\n'+date_time(1), fontsize=17)
+    plt.xlabel(xlabel, fontsize=15)
+    plt.ylabel('Loss', fontsize=15)
+    plt.ylim(min_y, max_y)
+    plt.legend(legends, loc='upper left')
+    plt.grid()
+    if figure_directory:
+        plt.savefig(figure_directory+"/history")
+
+    plt.show()
+
+# Setting variables for later use
+data = []
+labels = []
+classes = 43
+cur_path = os.getcwd()
+
+# Retrieving the images and their labels
+for i in range(classes):
+    path = os.path.join('images/','train',str(i))
+    images = os.listdir(path)
+
+    for a in images:
+        try:
+            image = Image.open(path + '/'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
+        except:
+            print("Error loading image")
+
+# Converting lists into numpy arrays
+data = np.array(data)
+labels = np.array(labels)
+
+# Checking data shape
+print(data.shape, labels.shape)
+
+# Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+# Displaying the shape after the split
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# Converting the labels into one hot encoding
+y_train = to_categorical(y_train, 43)
+y_test = to_categorical(y_test, 43)
+
+model_type = 1
+
+if(model_type == 1):
+
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+    model.add(Conv2D(filters=64, kernel_size=(5,5), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.15))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.20))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(rate=0.25))
+    model.add(Dense(43, activation='softmax'))
+
+elif(model_type == 2):
+
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+    model.add(Conv2D(filters=64, kernel_size=(5,5), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.15))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(rate=0.25))
+    model.add(Dense(43, activation='softmax'))
+
+elif(model_type == 3):
+
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(43, activation='softmax'))
+
+else:
+    print("NO MODEL SELECTED")
+
+# Compilation of the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#Model display
+model.summary()
+
+visualkeras.layered_view(model).show()
+visualkeras.layered_view(model, to_file='output.png')
+visualkeras.layered_view(model, to_file='output.png').show()
+
+with tf.device('/GPU:0'):
+    epochs = 5
+    history1 = model.fit(X_train, y_train, batch_size=128, epochs=epochs, validation_data=(X_test, y_test))
+
+plot_performance(history=history1)
+
+# Predict the classes
+y_pred = model.predict(X_test)
+y_pred_class = np.argmax(y_pred, axis=1)
+
+# Print the classification report
+print(classification_report(np.argmax(y_test, axis=1), y_pred_class))
+
